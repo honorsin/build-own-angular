7be3ad954c6ba999437f75c5c8e8a1d5ce13fd09
keshawn <15510010213@163.com>
2021-03-10 07:24:52 +0800

    [chapter-1]

--- src/scope.js
+++ src/scope.js
@@ -3,11 +3,12 @@
 function Scope () {
   this.$$watchers = [];
 }
-
+function initWatchVal() { }
 Scope.prototype.$watch = function (watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn, 
     listenerFn: listenerFn,
+    last: initWatchVal,
   };
   this.$$watchers.push(watcher);
 };
@@ -20,7 +21,11 @@ Scope.prototype.$digest = function () {
     oldValue = watcher.last;
     if (newValue !== oldValue) {
       watcher.last = newValue;
-      watcher.listenerFn(newValue, oldValue, self);
+      watcher.listenerFn(
+        newValue,
+        oldValue === initWatchVal ? newValue : oldValue,
+        self
+      );
     }
   });
 };
