a6db743ec259357cd98093babfb7cac3948910d6
kecui <kecui@ebay.com>
2021-03-09 19:19:37 +0800

    [chapter-1] add $$watcer

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1,10 +1,63 @@
-/* jshint globalstrict: true *//* global Scope: false */
-'use strict'; 
+/* jshint globalstrict: true */ /* global Scope: false */
+'use strict';
 
-describe("Scope", function () { 
-  it("can be constructed and used as an object", function () { 
-    var scope = new Scope(); 
+describe('Scope', function () {
+  it('can be constructed and used as an object', function () {
+    var scope = new Scope();
     scope.aProperty = 1;
-    expect(scope.aProperty).toBe(1); 
-  }); 
+
+    expect(scope.aProperty).toBe(1);
+  });
+
+  describe('digest', function () {
+    var scope;
+
+    beforeEach(function () {
+      scope = new Scope();
+    });
+
+    it('calls the listener function of a watch on first $digest', function () {
+      var watchFn = function () {
+        return 'wat';
+      };
+      var listenerFn = jasmine.createSpy();
+      scope.$watch(watchFn, listenerFn);
+      scope.$digest();
+
+      expect(listenerFn).toHaveBeenCalled();
+    });
+
+    it('calls the watch function of a watch on first $digest', function () {
+      var watchFn = jasmine.createSpy();
+      var listenerFn = function () {};
+      scope.$watch(watchFn, listenerFn);
+      scope.$digest();
+
+      expect(listenerFn).toHaveBeenCalled(scope);
+    });
+
+    it('calls the listener function when the watched value changes', function () {
+      scope.someValue = 'a';
+      scope.counter = 0;
+
+      scope.$watch(
+        function (scope) {
+          return scope.someValues;
+        },
+        function (newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+
+      expect(scope.counter).toBe(0);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.someValue = 'b';
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+    });
+  });
 });
